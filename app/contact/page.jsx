77d7c653244d011fdd4403823c3f5da016a5b565
UGC2885 <lucas_unzer@hotmail.com>
2025-02-06 16:14:16 -0300

    Mudado documento para conter o link do portfólio

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -46,7 +46,7 @@ const Contact = () => {
               {/* Botões de Download */}
               <div className="flex gap-4">
                 <a
-                  href="/curriculo/Lucas-Unzer-2025-portugues.pdf"
+                  href="/curriculo/Lucas-Port.pdf"
                   download
                   className="flex items-center gap-2 px-4 py-2 bg-accent text-white rounded-lg hover:bg-accent/80 transition"
                 >
@@ -54,7 +54,7 @@ const Contact = () => {
                 </a>
 
                 <a
-                  href="/curriculo/Lucas-Unzer-2025-english.pdf"
+                  href="/curriculo/Lucas-English.pdf"
                   download
                   className="flex items-center gap-2 px-4 py-2 bg-accent text-white rounded-lg hover:bg-accent/80 transition"
                 >
